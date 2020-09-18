21755e9f97125d693f29afbe1fa93b927a8db196
Ivan <vegarivann@gmail.com>
2020-09-18 21:06:47 +0200

    dodavanje emojia

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
 
 export default function Input(props) {
   const [value, setValue] = useState("");
+  const [showEmojis, setShowEmojis] = useState(false);
 
   const ChangeText = (event) => {
     setValue(event.target.value);
   };
 
+  // EMOJIS
+  const showEmojis2 = () => {
+    if (showEmojis === false) {
+      return setShowEmojis(true);
+    } else if (showEmojis === true) {
+      return setShowEmojis(false);
+    }
+  };
+
+  const addEmoji = (e) => {
+    let emoji = e.native;
+    setValue(value + emoji);
+  };
+
   const onSubmit = (event) => {
     event.preventDefault();
     setValue("");
+    setShowEmojis(false);
     props.onSendMessage(value);
   };
 
@@ -25,6 +43,18 @@ export default function Input(props) {
         />
         <button disabled={!value}>Sumbit</button>
       </form>
+
+      <span>
+        <button className="showandclosebotun" onClick={showEmojis2}>
+          {String.fromCodePoint(0x1f60a)}
+        </button>
+
+        {showEmojis && (
+          <span className="getEmojiButton">
+            <Picker onSelect={addEmoji} />
+          </span>
+        )}
+      </span>
     </div>
   );
 }
